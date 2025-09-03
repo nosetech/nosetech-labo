4f34834b0781804e423f5b864bc742a517cf506c
nosetech <system.nosetech@gmail.com>
2025-09-03 10:59:56 +0900

    react, eslint環境追加

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import js from '@eslint/js';
+import tsEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import astroEslint from 'eslint-plugin-astro';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs,ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsEslint,
+    },
+    rules: {
+      ...tsEslint.configs.recommended.rules,
+    },
+  },
+  ...astroEslint.configs.recommended,
+  {
+    ignores: ['node_modules/**', 'dist/**', '.astro/**'],
+  },
+];
